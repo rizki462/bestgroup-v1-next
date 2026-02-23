9968a9bb3429e442e427571a9c7bfa170bc2679d
Rizki Kurnia <rizkidiego614@gmail.com>
2026-02-23 20:48:51 +0700

    Add Pagination Features and Limit Features with Sample Data Account

--- src/app/(dashboard)/dashboard/user/_components/user.tsx
+++ src/app/(dashboard)/dashboard/user/_components/user.tsx
@@ -1,44 +1,49 @@
-"use client";
+'use client';
 
-import DataTable from "@/components/common/data-table";
-import DropdownAction from "@/components/common/dropdown-action";
-import { Button } from "@/components/ui/button";
-import { Dialog, DialogTrigger } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { HEADER_TABLE_USER } from "@/constants/user-constant";
-import { createClient } from "@/lib/supabase/client";
-import { useQuery } from "@tanstack/react-query";
-import { Pencil, Trash2 } from "lucide-react";
-import { useMemo } from "react";
-import { toast } from "sonner";
+import DataTable from '@/components/common/data-table';
+import DropdownAction from '@/components/common/dropdown-action';
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { HEADER_TABLE_USER } from '@/constants/user-constant';
+import useDataTable from '@/hooks/use-data-table';
+import { createClient } from '@/lib/supabase/client';
+import { useQuery } from '@tanstack/react-query';
+import { Pencil, Trash2 } from 'lucide-react';
+import { useMemo } from 'react';
+import { toast } from 'sonner';
 
 export default function UserManagement() {
   const supabase = createClient();
+  const { currentPage, currentLimit, handleChangePage, handleChangeLimit } =
+    useDataTable();
   const { data: users, isLoading } = useQuery({
-    queryKey: ["users"],
+    queryKey: ['users', currentPage, currentLimit],
     queryFn: async () => {
-      const { data, error } = await supabase
-        .from("profiles")
-        .select("*", { count: "exact" })
-        .order("created_at", { ascending: false });
+      const result = await supabase
+        .from('profiles')
+        .select('*', { count: 'exact' })
+        .range((currentPage - 1) * currentLimit, currentPage * currentLimit - 1)
+        .order('created_at');
 
-      if (error)
-        toast.error("Get Users Data Failed", {
-          description: error.message,
+      if (result.error)
+        toast.error('Get User data failed', {
+          description: result.error.message,
         });
 
-      return data;
+      return result;
     },
   });
 
   const filteredData = useMemo(() => {
-    return (users || []).map((user, index) => {
-        return [
-            index + 1,
-            user.id,
-            user.name,
-            user.role,
-            <DropdownAction menu={[
+    return (users?.data || []).map((user, index) => {
+      return [
+        index + 1,
+        user.id,
+        user.name,
+        user.role,
+        <DropdownAction
+          menu={[
             {
               label: (
                 <span className="flex item-center gap-2">
@@ -58,9 +63,16 @@ export default function UserManagement() {
               variant: 'destructive',
               action: () => {},
             },
-          ]} />
-        ]
+          ]}
+        />,
+      ];
     });
+  }, [users]);
+
+  const totalPages = useMemo(() => {
+    return users && users.count !== null
+      ? Math.ceil(users.count / currentLimit)
+      : 0;
   }, [users]);
 
   return (
@@ -71,12 +83,21 @@ export default function UserManagement() {
           <Input placeholder="Search by name" />
           <Dialog>
             <DialogTrigger asChild>
-              <Button variant="outline">Add User</Button>
+              <Button variant="outline">Create</Button>
             </DialogTrigger>
           </Dialog>
         </div>
       </div>
-      <DataTable header={HEADER_TABLE_USER} isLoading={isLoading} data={filteredData} />
+      <DataTable
+        header={HEADER_TABLE_USER}
+        data={filteredData}
+        isLoading={isLoading}
+        totalPages={totalPages}
+        currentPage={currentPage}
+        currentLimit={currentLimit}
+        onChangePage={handleChangePage}
+        onChangeLimit={handleChangeLimit}
+      />
     </div>
   );
 }
--- src/components/common/data-table.tsx
+++ src/components/common/data-table.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from "react";
-import { Card } from "../ui/card";
+import { ReactNode } from 'react';
+import { Card } from '../ui/card';
 import {
   Table,
   TableBody,
@@ -7,16 +7,38 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "../ui/table";
+} from '../ui/table';
+import PaginationDataTable from './pagination-data-table';
+import { Label } from '../ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select';
+import { LIMIT_LISTS } from '@/constants/data-table-constant';
 
 export default function DataTable({
   header,
   data,
   isLoading,
+  totalPages,
+  currentPage,
+  currentLimit,
+  onChangePage,
+  onChangeLimit,
 }: {
   header: string[];
   data: (string | ReactNode)[][];
   isLoading?: boolean;
+  totalPages: number;
+  currentPage: number;
+  currentLimit: number;
+  onChangePage: (page: number) => void;
+  onChangeLimit: (limit: number) => void;
 }) {
   return (
     <div className="w-full flex flex-col gap-4">
@@ -24,9 +46,9 @@ export default function DataTable({
         <Table className="w-full rounded-lg overflow-hidden">
           <TableHeader className="bg-muted sticky top-0 z-10">
             <TableRow>
-              {header.map((item) => (
-                <TableHead key={`th-${item}`} className="px-6 py-3">
-                  {item}
+              {header.map((column) => (
+                <TableHead key={`th-${column}`} className="px-6 py-3">
+                  {column}
                 </TableHead>
               ))}
             </TableRow>
@@ -34,25 +56,23 @@ export default function DataTable({
           <TableBody>
             {data?.map((row, rowIndex) => (
               <TableRow key={`tr-${rowIndex}`}>
-                {row.map((collumn, collumnIndex) => (
+                {row.map((column, columnIndex) => (
                   <TableCell
-                    key={`td-${rowIndex}-${collumnIndex}`}
                     className="px-6 py-3"
+                    key={`tc-${rowIndex}-${columnIndex}`}
                   >
-                    {collumn}
+                    {column}
                   </TableCell>
                 ))}
               </TableRow>
             ))}
-
             {data?.length === 0 && !isLoading && (
               <TableRow>
                 <TableCell colSpan={header.length} className="h-24 text-center">
-                  No results data.
+                  No Result Data
                 </TableCell>
               </TableRow>
             )}
-
             {isLoading && (
               <TableRow>
                 <TableCell colSpan={header.length} className="h-24 text-center">
@@ -60,10 +80,41 @@ export default function DataTable({
                 </TableCell>
               </TableRow>
             )}
-
           </TableBody>
         </Table>
       </Card>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Label>Limit</Label>
+          <Select
+            value={currentLimit.toString()}
+            onValueChange={(value) => onChangeLimit(Number(value))}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select Limit" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Limit</SelectLabel>
+                {LIMIT_LISTS.map((limit) => (
+                  <SelectItem key={limit} value={limit.toString()}>
+                    {limit}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        {totalPages > 1 && (
+          <div className="flex justify-end">
+            <PaginationDataTable
+              currentPage={currentPage}
+              onChangePage={onChangePage}
+              totalPages={totalPages}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
